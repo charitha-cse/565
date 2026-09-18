a7b545735e17b7fcd0b7e3d18f672574fa54df75
charitha-cse <242p1a0565@cbit.edu.in>
2026-09-18 11:38:59 +0530

    Change server port from 3000 to 3004

--- app/express4.js
+++ app/express4.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mysql from 'mysql';
 
 const app = express();
-const port = 3000;
+const port = 3004;
 
 // Create connection
 const db = mysql.createConnection({
