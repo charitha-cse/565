21e84f07d1a25f27f35c0d47be5a90442118877c
charitha-cse <242p1a0565@cbit.edu.in>
2026-09-18 11:31:20 +0530

    Add a root route to serve HTML content

--- app/express2.js
+++ app/express2.js
@@ -3,6 +3,23 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+    res.send(`
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>HELLO WORLD</title>
+            </head>
+            <body>
+                <h1>WELCOME TO EXPRESS.JS</h1>
+                <script>
+                    console.log('hello world');
+                </script>
+            </body>
+        </html>
+    `);
+});
+
 app.listen(port, () => {
-    console.log("Hello World printed in the console!");
+    console.log(`server running at http://localhost:${port}`);
 });
